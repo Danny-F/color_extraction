83d3ad25ea3169af92381581e6cfdc24b4beacae
Danny <dannyfarrington5@gmail.com>
2020-08-09 10:10:51 -0500

    re organizing color extraction script into functions

--- streamlit_color_extraction.py
+++ streamlit_color_extraction.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Jun 28 17:22:32 2020
-
-@author: dannyfarrington
-"""
 
 
 from matplotlib import pyplot as plt
@@ -18,23 +11,35 @@ from skimage.color import lab2rgb
 import streamlit as st
 import itertools
 
+
+############### functions ###############
+
 #streamlit sidebar variables
-st.title('Extract Colors From An Image')
-image_link = st.sidebar.text_input('Image Link', 'paste image link here')
-inertia_change_threshold = st.sidebar.slider('Inertia % Change Threshold', -100, -1, -25)
-ignore_background = st.sidebar.checkbox('Ignore background color?', value=True)
+def collect_user_entered_variables():
+    st.title('Extract Colors From An Image')
+    image_link = st.sidebar.text_input('Image Link', 'paste image link here')
+    inertia_change_threshold = st.sidebar.slider('Inertia % Change Threshold', -100, -1, -25)
+    ignore_background = st.sidebar.checkbox('Ignore background color?', value=True)
+    user_entered_variables = {'image_link': image_link,
+                            'inertia_change_threshold': inertia_change_threshold,
+                            'ignore_background': ignore_background}
+    return user_entered_variables
 
-if image_link != 'paste image link here':
 
-    st.image(image_link, caption = 'normal image')
+#display original image
+def display_original_image(image_link):
+    st.image(user_variables['image_link'], caption = 'normal image')
 
-    #reading in image and generating l*a*b version as well
+
+#reading in image and generating l*a*b version as well
+def import_image(image_link):
     image_rgb = io.imread(image_link)
     image_cie = rgb2lab(image_rgb)
     image_shape = image_rgb.shape
+    return image_rgb, image_cie, image_shape
 
-
-    #creating df that has the cie and rgb values for each pixel
+#create df that has the cie and rgb values for each pixel
+def format_image_data_into_dataframe(image_rgb, image_cie, ignore_background):
     rgb_list = []
     cie_list = []
     for x,_ in enumerate(image_rgb):
@@ -49,12 +54,14 @@ if image_link != 'paste image link here':
     df['rgb_comb'] = df['r']*1000000 + df['g']*1000 + df['b']
     background = df['rgb_comb'].value_counts().index[0]
     if ignore_background == False:
-        background = 100000000000 # this won't make to any RGB combo
-    nobg_index = df['rgb_comb']!=background
-    nobg_df = df[nobg_index].copy()
+        background = 100000000000 # this won't match to any RGB combo
+    no_background_index = df['rgb_comb']!=background
+    no_background_df = df[no_background_index].copy()
+    background_df = df[~no_background_index].copy()
+    return no_background_df, background_df
 
-
-    #finding the ideal cluster number
+#determing the ideal cluster number for clustering pixels
+def determine_ideal_cluster_number(image_data_df, inertia_change_threshold):
     chart = st.line_chart()
     i=0
     large_change = True
@@ -63,53 +70,79 @@ if image_link != 'paste image link here':
     while large_change == True:
         i += 1
         km = MiniBatchKMeans(n_clusters = i)
-        km = km.fit(nobg_df[['c', 'i', 'e']])
+        km = km.fit(image_data_df[['c', 'i', 'e']])
         new_inertia = km.inertia_
         change = (new_inertia-prev_inertia)/prev_inertia
         large_change = change < inertia_change_threshold/100
         prev_inertia = new_inertia
         chart.add_rows([prev_inertia])
+    ideal_cluster_num = i - 1
+    return ideal_cluster_num
 
-    #using the ideal cluster number to find the individual color info
-    ideal_cluster_num = i-1
+#assigning pixels to clusters
+def assign_clusters(image_data_df, ideal_cluster_num):
     st.write('Ideal cluster number: {}'.format(ideal_cluster_num))
     km = KMeans(n_clusters = ideal_cluster_num)
     km = km.fit(nobg_df[['c', 'i', 'e']])
-    nobg_df['label'] = km.labels_
+    return km.labels_
 
-    label_grouped_df = nobg_df.groupby('label')[['r', 'g', 'b']].mean().round(0).astype(int)
-    label_grouped_df['avg_rgb'] = label_grouped_df[['r', 'g', 'b']].apply(lambda row: np.asarray([row[0], row[1], row[2]], dtype='uint8'), axis=1)
-    label_grouped_df['rgb_plt_color'] = label_grouped_df[['r', 'g', 'b']].apply(lambda row: [row[0]/255, row[1]/255, row[2]/255], axis=1)
-    label_grouped_df['count'] = nobg_df['label'].value_counts()
-    label_grouped_df['percent'] = round(label_grouped_df['count']/ label_grouped_df['count'].sum(),2)
-    label_grouped_df = label_grouped_df.sort_values(['count'], ascending = False).reset_index()
+#average data by cluster cluster
+def avg_data_by_cluster(image_data_df):
+    cluster_grouped_df = image_data_df.groupby('cluster')[['r', 'g', 'b']].mean().round(0).astype(int)
+    cluster_grouped_df['avg_rgb'] = cluster_grouped_df[['r', 'g', 'b']].apply(lambda row: np.asarray([row[0], row[1], row[2]], dtype='uint8'), axis=1)
+    cluster_grouped_df['rgb_plt_color'] = cluster_grouped_df[['r', 'g', 'b']].apply(lambda row: [row[0]/255, row[1]/255, row[2]/255], axis=1)
+    cluster_grouped_df['count'] = image_data_df['cluster'].value_counts()
+    cluster_grouped_df['percent'] = round(cluster_grouped_df['count']/ cluster_grouped_df['count'].sum(),2)
+    cluster_grouped_df = cluster_grouped_df.sort_values(['count'], ascending = False).reset_index()
+    return cluster_grouped_df
 
-
-    # displaying the pie chart of the extracted colors
-    plt.pie(label_grouped_df['count'],
-            colors=label_grouped_df['rgb_plt_color'].tolist()
+#displaying the pie chart of the clustered colors
+def display_color_pie_graph(cluster_grouped_df):
+    plt.pie(cluster_grouped_df['count'],
+            colors=cluster_grouped_df['rgb_plt_color'].tolist()
             )
     st.pyplot()
 
-    #joining the avgeraged rgb values back into the full list of pixels
-    nobg_df = nobg_df.reset_index()
-    nobg_df = pd.merge(nobg_df, label_grouped_df[['label','avg_rgb']], how='left', on='label')
-    nobg_df.set_index(nobg_df['index'], inplace=True)
-    df['avg_rgb'] = nobg_df['avg_rgb']
-    df.loc[~nobg_index, 'avg_rgb'] = df.loc[~nobg_index, 'rgb']
+#joining the avgeraged rgb values back into the full list of pixels
+def join_cluster_data_to_full_image_data(image_data_df, cluster_grouped_df, background_df):
+    full_image_data_df = pd.concat([image_data_df,background_df])
+    full_image_data_df = full_image_data_df.reset_index()
+    full_image_data_df = pd.merge(full_image_data_df, cluster_grouped_df[['cluster','avg_rgb']], how='left', on='cluster')
+    full_image_data_df.set_index(full_image_data_df['index'], inplace=True)
+    #for the background color pixels, assign their rgb value to the avg_rgb since they are equal
+    np.where(full_image_data_df['avg_rgb'].isnull(), full_image_data_df['rgb'], full_image_data_df['avg_rgb'])
+    return full_image_data_df
 
-    #creating the new image with the averaged colors
-    image_avg_rgb = np.asarray(df['avg_rgb'].tolist()).reshape(image_shape)
+#displaying the background color that was ignored, if "Ignore background color" was checked
+def display_background_color(background_df):
+    bg_rgb = background_df.iloc[0]['rgb']
+    bg_image = np.asarray(list(itertools.repeat(bg_rgb, 2000))).reshape(20,100,3)
+    st.write('Background Color')
+    st.image(bg_image)
 
-    # displaying the background color that was ignored, if "Ignore background color" was checked
-    if ignore_background == True:
-        bg_rgb = df[nobg_index==False].iloc[0]['rgb']
-        image_bg = np.asarray(list(itertools.repeat(bg_rgb, 2000))).reshape(20,100,3)
-        st.write('Background Color')
-        st.image(image_bg)
+#create and display the new image with the averaged colors
+def display_image_with_clustered_colors(image_shape):
+        image_avg_rgb = np.asarray(full_image_data_df['avg_rgb'].tolist()).reshape(image_shape)
+        st.image(image_avg_rgb, caption = 'clustered colors')
 
-    #dsiplaying the image with the averaged colors
-    st.image(image_avg_rgb, caption = 'clustered colors')
+############### execution ###############
 
+user_variables = collect_user_entered_variables()
+
+if user_variables['image_link'] != 'paste image link here':
+    display_original_image(user_variables['image_link'])
+    image_rgb, image_cie, image_shape = import_image(user_variables['image_link'])
+    image_data_df, background_df = format_image_data_into_dataframe(image_rgb, image_cie,
+                                                                    user_variables['ignore_background'])
+    ideal_cluster_num = determine_ideal_cluster_number(image_data_df,
+                                                       user_variables['inertia_change_threshold'])
+    image_data_df['cluster'] = assign_clusters(image_data_df, ideal_cluster_num)
+    cluster_grouped_df = avg_data_by_cluster(image_data_df)
+    display_color_pie_graph(cluster_grouped_df)
+    full_image_data_df = join_cluster_data_to_full_image_data(image_data_df, background_df,
+                                                              cluster_grouped_df)
+    if user_variables['ignore_background'] == True:
+        display_background_color(background_df)
+    display_image_with_clustered_colors(full_image_data_df, image_shape)
 else:
     st.write('Past an image link (no quotes needed) into the image link field in the sidebar.')
